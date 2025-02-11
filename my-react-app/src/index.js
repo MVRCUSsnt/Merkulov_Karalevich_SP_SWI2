93e1ae9b48654f60956c7401556bb0a016d7adaa
NITRO <ya.mass.effect.3@gmail.com>
2025-02-11 10:20:41 +0100

    Исправил баг с авторизацией

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import '../my-react-app/src/index.css';
+import App from '../my-react-app/src/App';
+import reportWebVitals from "../my-react-app/src/reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
