003906d4ecb9e9a1ce57e55d0c33efe5f140e609
MVRCUS <Ya.mass.effect.3@gmail.com>
2025-12-22 19:57:23 +0100

    Cleanup: remove node_modules, IDE files and leftover React apps

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ChatApp from './js/ChatApp';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<ChatApp />);
-
